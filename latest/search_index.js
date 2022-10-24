6a22bc93a220bce4e5e8e545310a855b5b5e62aa
Documenter.jl <documenter@juliadocs.github.io>
2022-10-24 17:49:52 +0000

    build based on c44a030

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TheGraphEnvironments","category":"page"},{"location":"#TheGraphEnvironments","page":"Home","title":"TheGraphEnvironments","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TheGraphEnvironments.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TheGraphEnvironments]","category":"page"}]
+}
